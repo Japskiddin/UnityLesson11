17a98f12f65a141106f90b50fa5b98cacffaf019
Nikita <japskiddin@gmail.com>
2021-11-13 15:48:55 +0800

    Completed

--- Assets/Scripts/SettingsPopup.cs
+++ Assets/Scripts/SettingsPopup.cs
@@ -14,4 +14,29 @@ public class SettingsPopup : MonoBehaviour
     public void OnSoundValue(float volume) { // ползунок регулирует свойство volume диспетчера управления звуком
         Managers.Audio.soundVolume = volume;
     }
+
+    public void OnMusicToggle() {
+        Managers.Audio.musicMute = !Managers.Audio.musicMute;
+        Managers.Audio.PlaySound(sound);
+    }
+
+    public void OnMusicValue(float volume) {
+        Managers.Audio.musicVolume = volume;
+    }
+
+    public void OnPlayMusic(int selector) { // этот метод получает от кнопки численных параметр
+        Managers.Audio.PlaySound(sound);
+
+        switch (selector) { // вызываем для каждой кнопки свою музыкальную функцию в диспетчере AudioManager
+            case 1:
+                Managers.Audio.PlayIntroMusic();
+                break;
+            case 2:
+                Managers.Audio.PlayLevelMusic();
+                break;
+            default:
+                Managers.Audio.StopMusic();
+                break;
+        }
+    }
 }
